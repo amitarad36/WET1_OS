3ac61162e1624cd65092fdd7b1f79efdb3c0b9f1
amitarad36 <129197826+amitarad36@users.noreply.github.com>
2024-12-15 10:58:36 +0100

    vers1

--- skeleton_smash/Commands.cpp
+++ skeleton_smash/Commands.cpp
@@ -1,117 +1,122 @@
-#include <unistd.h>
-#include <string.h>
-#include <iostream>
-#include <vector>
-#include <sstream>
-#include <sys/wait.h>
-#include <iomanip>
+
 #include "Commands.h"
+#include <unistd.h>
+#include <sys/wait.h>
+#include <iostream>
+#include <cstring>
+#include <csignal>
 
-using namespace std;
+Command::Command(const char* cmd_line) : m_cmd_line(cmd_line) {}
+Command::~Command() {}
 
-const std::string WHITESPACE = " \n\r\t\f\v";
-
-#if 0
-#define FUNC_ENTRY()  \
-  cout << __PRETTY_FUNCTION__ << " --> " << endl;
-
-#define FUNC_EXIT()  \
-  cout << __PRETTY_FUNCTION__ << " <-- " << endl;
-#else
-#define FUNC_ENTRY()
-#define FUNC_EXIT()
-#endif
-
-string _ltrim(const std::string &s) {
-    size_t start = s.find_first_not_of(WHITESPACE);
-    return (start == std::string::npos) ? "" : s.substr(start);
+std::string Command::getCommandLine() const {
+    return std::string(m_cmd_line);
 }
 
-string _rtrim(const std::string &s) {
-    size_t end = s.find_last_not_of(WHITESPACE);
-    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
-}
+BuiltInCommand::BuiltInCommand(const char* cmd_line) : Command(cmd_line) {}
+BuiltInCommand::~BuiltInCommand() {}
 
-string _trim(const std::string &s) {
-    return _rtrim(_ltrim(s));
-}
-
-int _parseCommandLine(const char *cmd_line, char **args) {
-    FUNC_ENTRY()
-    int i = 0;
-    std::istringstream iss(_trim(string(cmd_line)).c_str());
-    for (std::string s; iss >> s;) {
-        args[i] = (char *) malloc(s.length() + 1);
-        memset(args[i], 0, s.length() + 1);
-        strcpy(args[i], s.c_str());
-        args[++i] = NULL;
+ExternalCommand::ExternalCommand(const char* cmd_line) : Command(cmd_line) {
+    m_isBackground = (m_cmdLine.back() == '&');
+    if (m_isBackground) {
+        m_cmdLine.pop_back();
     }
-    return i;
-
-    FUNC_EXIT()
 }
+ExternalCommand::~ExternalCommand() {}
 
-bool _isBackgroundComamnd(const char *cmd_line) {
-    const string str(cmd_line);
-    return str[str.find_last_not_of(WHITESPACE)] == '&';
-}
-
-void _removeBackgroundSign(char *cmd_line) {
-    const string str(cmd_line);
-    // find last character other than spaces
-    unsigned int idx = str.find_last_not_of(WHITESPACE);
-    // if all characters are spaces then return
-    if (idx == string::npos) {
-        return;
+void ExternalCommand::execute() {
+    pid_t pid = fork();
+    if (pid == 0) { // Child process
+        setpgrp();
+        execlp(m_cmdLine.c_str(), m_cmdLine.c_str(), (char*)NULL);
+        perror("Exec failed");
+        exit(1);
+    } else if (pid > 0) { // Parent process
+        SmallShell& shell = SmallShell::getInstance();
+        if (m_isBackground) {
+            shell.getJobsList().addJob(pid, m_cmdLine, false);
+            std::cout << "Background job started with PID: " << pid << std::endl;
+        } else {
+            shell.setForegroundJob(pid, m_cmdLine);
+            waitpid(pid, nullptr, 0);
+            shell.clearForegroundJob();
+        }
+    } else {
+        perror("smash error: fork failed");
     }
-    // if the command line does not end with & then return
-    if (cmd_line[idx] != '&') {
-        return;
+}
+
+JobsList::JobEntry::JobEntry(int jobId, const std::string& command, int pid, bool isStopped)
+    : m_jobId(jobId), m_command(command), m_pid(pid), m_isStopped(isStopped) {}
+
+JobsList::JobsList() : m_lastJobId(0) {}
+JobsList::~JobsList() {}
+
+void JobsList::addJob(int pid, const std::string& command, bool isStopped) {
+    removeFinishedJobs();
+    m_jobs.emplace_back(++m_lastJobId, command, pid, isStopped);
+}
+
+void JobsList::removeFinishedJobs() {
+    for (auto it = m_jobs.begin(); it != m_jobs.end();) {
+        int status;
+        if (waitpid(it->m_pid, &status, WNOHANG) > 0) {
+            it = m_jobs.erase(it);
+        } else {
+            ++it;
+        }
     }
-    // replace the & (background sign) with space and then remove all tailing spaces.
-    cmd_line[idx] = ' ';
-    // truncate the command line string up to the last non-space character
-    cmd_line[str.find_last_not_of(WHITESPACE, idx) + 1] = 0;
 }
 
-// TODO: Add your implementation for classes in Commands.h 
-
-SmallShell::SmallShell() {
-// TODO: add your implementation
+void JobsList::printJobsList() const {
+    for (const auto& job : m_jobs) {
+        std::cout << "[" << job.m_jobId << "] " << job.m_command
+                  << (job.m_isStopped ? " (stopped)" : "") << std::endl;
+    }
 }
 
-SmallShell::~SmallShell() {
-// TODO: add your implementation
+void JobsList::killAllJobs() {
+    for (const auto& job : m_jobs) {
+        kill(job.m_pid, SIGKILL);
+        std::cout << job.m_pid << ": " << job.m_command << std::endl;
+    }
+    m_jobs.clear();
 }
 
-/**
-* Creates and returns a pointer to Command class which matches the given command line (cmd_line)
-*/
-Command *SmallShell::CreateCommand(const char *cmd_line) {
-    // For example:
-  /*
-  string cmd_s = _trim(string(cmd_line));
-  string firstWord = cmd_s.substr(0, cmd_s.find_first_of(" \n"));
+SmallShell::SmallShell() : m_prompt("smash"), m_foregroundPid(-1), m_foregroundCommand("") {}
+SmallShell::~SmallShell() {}
 
-  if (firstWord.compare("pwd") == 0) {
-    return new GetCurrDirCommand(cmd_line);
-  }
-  else if (firstWord.compare("showpid") == 0) {
-    return new ShowPidCommand(cmd_line);
-  }
-  else if ...
-  .....
-  else {
-    return new ExternalCommand(cmd_line);
-  }
-  */
-    return nullptr;
+SmallShell& SmallShell::getInstance() {
+    static SmallShell instance;
+    return instance;
 }
 
-void SmallShell::executeCommand(const char *cmd_line) {
-    // TODO: Add your implementation here
-    // for example:
-    // Command* cmd = CreateCommand(cmd_line);
-    // cmd->execute();
-    // Please note that you must fork smash process for some commands (e.g., external commands....)
+std::string SmallShell::getPrompt() const {
+    return m_prompt + "> ";
+}
+
+void SmallShell::setPrompt(const std::string& prompt) {
+    m_prompt = prompt;
+}
+
+void SmallShell::setForegroundJob(int pid, const std::string& command) {
+    m_foregroundPid = pid;
+    m_foregroundCommand = command;
+}
+
+void SmallShell::clearForegroundJob() {
+    m_foregroundPid = -1;
+    m_foregroundCommand.clear();
+}
+
+int SmallShell::getForegroundPid() const {
+    return m_foregroundPid;
+}
+
+std::string SmallShell::getForegroundCommand() const {
+    return m_foregroundCommand;
+}
+
+JobsList& SmallShell::getJobsList() {
+    return m_jobsList;
 }
--- skeleton_smash/Commands.h
+++ skeleton_smash/Commands.h
@@ -1,246 +1,85 @@
+
 #ifndef SMASH_COMMAND_H_
 #define SMASH_COMMAND_H_
 
+#include <string>
 #include <vector>
 
 #define COMMAND_MAX_LENGTH (200)
 #define COMMAND_MAX_ARGS (20)
 
 class Command {
-    // TODO: Add your data members
+protected:
+    const char* m_cmd_line;
+
 public:
-    Command(const char *cmd_line);
-
+    Command(const char* cmd_line);
     virtual ~Command();
-
     virtual void execute() = 0;
-
-    //virtual void prepare();
-    //virtual void cleanup();
-    // TODO: Add your extra methods if needed
+    std::string getCommandLine() const;
 };
 
 class BuiltInCommand : public Command {
 public:
-    BuiltInCommand(const char *cmd_line);
-
-    virtual ~BuiltInCommand() {
-    }
+    BuiltInCommand(const char* cmd_line);
+    virtual ~BuiltInCommand();
 };
 
 class ExternalCommand : public Command {
+private:
+    std::string m_cmdLine;
+    bool m_isBackground;
+
 public:
-    ExternalCommand(const char *cmd_line);
-
-    virtual ~ExternalCommand() {
-    }
-
+    ExternalCommand(const char* cmd_line);
+    virtual ~ExternalCommand();
     void execute() override;
 };
-
-class PipeCommand : public Command {
-    // TODO: Add your data members
-public:
-    PipeCommand(const char *cmd_line);
-
-    virtual ~PipeCommand() {
-    }
-
-    void execute() override;
-};
-
-class RedirectionCommand : public Command {
-    // TODO: Add your data members
-public:
-    explicit RedirectionCommand(const char *cmd_line);
-
-    virtual ~RedirectionCommand() {
-    }
-
-    void execute() override;
-};
-
-class ChangeDirCommand : public BuiltInCommand {
-    // TODO: Add your data members public:
-    ChangeDirCommand(const char *cmd_line, char **plastPwd);
-
-    virtual ~ChangeDirCommand() {
-    }
-
-    void execute() override;
-};
-
-class GetCurrDirCommand : public BuiltInCommand {
-public:
-    GetCurrDirCommand(const char *cmd_line);
-
-    virtual ~GetCurrDirCommand() {
-    }
-
-    void execute() override;
-};
-
-class ShowPidCommand : public BuiltInCommand {
-public:
-    ShowPidCommand(const char *cmd_line);
-
-    virtual ~ShowPidCommand() {
-    }
-
-    void execute() override;
-};
-
-class JobsList;
-
-class QuitCommand : public BuiltInCommand {
-    // TODO: Add your data members public:
-    QuitCommand(const char *cmd_line, JobsList *jobs);
-
-    virtual ~QuitCommand() {
-    }
-
-    void execute() override;
-};
-
 
 class JobsList {
 public:
     class JobEntry {
-        // TODO: Add your data members
+    public:
+        int m_jobId;
+        std::string m_command;
+        int m_pid;
+        bool m_isStopped;
+
+        JobEntry(int jobId, const std::string& command, int pid, bool isStopped);
     };
 
-    // TODO: Add your data members
+private:
+    std::vector<JobEntry> m_jobs;
+    int m_lastJobId;
+
 public:
     JobsList();
-
     ~JobsList();
-
-    void addJob(Command *cmd, bool isStopped = false);
-
-    void printJobsList();
-
-    void killAllJobs();
-
+    void addJob(int pid, const std::string& command, bool isStopped);
     void removeFinishedJobs();
-
-    JobEntry *getJobById(int jobId);
-
-    void removeJobById(int jobId);
-
-    JobEntry *getLastJob(int *lastJobId);
-
-    JobEntry *getLastStoppedJob(int *jobId);
-
-    // TODO: Add extra methods or modify exisitng ones as needed
+    void printJobsList() const;
+    void killAllJobs();
 };
-
-class JobsCommand : public BuiltInCommand {
-    // TODO: Add your data members
-public:
-    JobsCommand(const char *cmd_line, JobsList *jobs);
-
-    virtual ~JobsCommand() {
-    }
-
-    void execute() override;
-};
-
-class KillCommand : public BuiltInCommand {
-    // TODO: Add your data members
-public:
-    KillCommand(const char *cmd_line, JobsList *jobs);
-
-    virtual ~KillCommand() {
-    }
-
-    void execute() override;
-};
-
-class ForegroundCommand : public BuiltInCommand {
-    // TODO: Add your data members
-public:
-    ForegroundCommand(const char *cmd_line, JobsList *jobs);
-
-    virtual ~ForegroundCommand() {
-    }
-
-    void execute() override;
-};
-
-class ListDirCommand : public Command {
-public:
-    ListDirCommand(const char *cmd_line);
-
-    virtual ~ListDirCommand() {
-    }
-
-    void execute() override;
-};
-
-class WhoAmICommand : public Command {
-public:
-    WhoAmICommand(const char *cmd_line);
-
-    virtual ~WhoAmICommand() {
-    }
-
-    void execute() override;
-};
-
-class NetInfo : public Command {
-    // TODO: Add your data members
-public:
-    NetInfo(const char *cmd_line);
-
-    virtual ~NetInfo() {
-    }
-
-    void execute() override;
-};
-
-class aliasCommand : public BuiltInCommand {
-public:
-    aliasCommand(const char *cmd_line);
-
-    virtual ~aliasCommand() {
-    }
-
-    void execute() override;
-};
-
-class unaliasCommand : public BuiltInCommand {
-public:
-    unaliasCommand(const char *cmd_line);
-
-    virtual ~unaliasCommand() {
-    }
-
-    void execute() override;
-};
-
 
 class SmallShell {
 private:
-    // TODO: Add your data members
+    std::string m_prompt;
+    JobsList m_jobsList;
+    int m_foregroundPid;
+    std::string m_foregroundCommand;
+
     SmallShell();
 
 public:
-    Command *CreateCommand(const char *cmd_line);
-
-    SmallShell(SmallShell const &) = delete; // disable copy ctor
-    void operator=(SmallShell const &) = delete; // disable = operator
-    static SmallShell &getInstance() // make SmallShell singleton
-    {
-        static SmallShell instance; // Guaranteed to be destroyed.
-        // Instantiated on first use.
-        return instance;
-    }
-
     ~SmallShell();
-
-    void executeCommand(const char *cmd_line);
-
-    // TODO: add extra methods as needed
+    static SmallShell& getInstance();
+    std::string getPrompt() const;
+    void setPrompt(const std::string& prompt);
+    void setForegroundJob(int pid, const std::string& command);
+    void clearForegroundJob();
+    int getForegroundPid() const;
+    std::string getForegroundCommand() const;
+    JobsList& getJobsList();
 };
 
-#endif //SMASH_COMMAND_H_
+#endif // SMASH_COMMAND_H_
